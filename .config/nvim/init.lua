4df16f42939c511e3e6ade9c1230e54ae5f0a2ba
Yong Su Lee <yslee3@icloud.com>
2024-01-11 14:54:08 -0500

    disable syntax highlighting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -107,6 +107,8 @@ require("lazy").setup({
             --- if you want to know more about lsp-zero and mason.nvim
             --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
             lsp_zero.on_attach(function(client, bufnr)
+                -- disable lsp syntax highlighting
+                client.server_capabilities.semanticTokensProvider = nil
                 -- see :help lsp-zero-keybindings
                 -- to learn the available actions
                 lsp_zero.default_keymaps({buffer = bufnr})
