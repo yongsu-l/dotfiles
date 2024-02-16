c8cb5245dccf23990d00730564d8b28d907a9253
Yong Su Lee <yslee3@icloud.com>
2024-02-16 18:34:39 -0500

    format and diffnvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,183 +1,183 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    { "RRethy/nvim-base16" },
-    {
-        'stevearc/oil.nvim',
-        config = function ()
-            require("oil").setup({
-                -- default_file_explorer = false
-            })
-        end
-    },
-    {
-        "ibhagwan/fzf-lua",
-        -- optional for icon support
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
-    {
-        {
-            "tpope/vim-fugitive",
-            config = function(_, _)
-                -- deprecated function that still tab completes
-                vim.api.nvim_del_user_command('Gbrowse')
-            end
-        },
-        { "tpope/vim-rhubarb" },
-        { "tpope/vim-unimpaired" },
-    },
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        lazy = true,
-        config = false,
-        init = function()
-            -- Disable automatic setup, we are doing it manually
-            vim.g.lsp_zero_extend_cmp = 0
-            vim.g.lsp_zero_extend_lspconfig = 0
-        end,
-    },
-    {
-        'williamboman/mason.nvim',
-        lazy = false,
-        config = true,
-    },
+	{ "RRethy/nvim-base16" },
+	{
+		"stevearc/oil.nvim",
+		config = function()
+			require("oil").setup({
+				-- default_file_explorer = false
+			})
+		end,
+	},
+	{
+		"ibhagwan/fzf-lua",
+		-- optional for icon support
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		{
+			"tpope/vim-fugitive",
+			dependencies = { "sindrets/diffview.nvim" },
+			config = function(_, _)
+				-- deprecated function that still tab completes
+				vim.api.nvim_del_user_command("Gbrowse")
+			end,
+		},
+		{ "tpope/vim-rhubarb" },
+		{ "tpope/vim-unimpaired" },
+	},
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		lazy = true,
+		config = false,
+		init = function()
+			-- Disable automatic setup, we are doing it manually
+			vim.g.lsp_zero_extend_cmp = 0
+			vim.g.lsp_zero_extend_lspconfig = 0
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = true,
+	},
 
-    -- Autocompletion
-    {
-        'hrsh7th/nvim-cmp',
-        event = 'InsertEnter',
-        dependencies = {
-            {'L3MON4D3/LuaSnip'},
-            { 'hrsh7th/cmp-buffer' },   -- Required
-            { 'hrsh7th/cmp-path' },     -- Required
-        },
-        config = function()
-            -- Here is where you configure the autocompletion settings.
-            local lsp_zero = require('lsp-zero')
-            lsp_zero.extend_cmp()
+	-- Autocompletion
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			{ "L3MON4D3/LuaSnip" },
+			{ "hrsh7th/cmp-buffer" }, -- Required
+			{ "hrsh7th/cmp-path" }, -- Required
+		},
+		config = function()
+			-- Here is where you configure the autocompletion settings.
+			local lsp_zero = require("lsp-zero")
+			lsp_zero.extend_cmp()
 
-            -- And you can configure cmp even more, if you want to.
-            local cmp = require('cmp')
+			-- And you can configure cmp even more, if you want to.
+			local cmp = require("cmp")
 
-            cmp.setup({
-                formatting = lsp_zero.cmp_format(),
-                mapping = cmp.mapping.preset.insert({
-                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                    ['<CR>'] = cmp.mapping.confirm({ select = false }),
-                }),
-                sources = {
-                    { name = 'path' },
-                    { name = 'buffer' },
-                    { name = 'nvim_lsp' },
-                }
-            })
-        end
-    },
+			cmp.setup({
+				formatting = lsp_zero.cmp_format(),
+				mapping = cmp.mapping.preset.insert({
+					["<C-u>"] = cmp.mapping.scroll_docs(-4),
+					["<C-d>"] = cmp.mapping.scroll_docs(4),
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+				}),
+				sources = {
+					{ name = "path" },
+					{ name = "buffer" },
+					{ name = "nvim_lsp" },
+				},
+			})
+		end,
+	},
 
-    -- LSP
-    {
-        'neovim/nvim-lspconfig',
-        cmd = {'LspInfo', 'LspInstall', 'LspStart'},
-        event = {'BufReadPre', 'BufNewFile'},
-        dependencies = {
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'williamboman/mason-lspconfig.nvim'},
-        },
-        config = function()
-            vim.lsp.set_log_level("off")
-            -- This is where all the LSP shenanigans will live
-            local lsp_zero = require('lsp-zero')
-            lsp_zero.extend_lspconfig()
+	-- LSP
+	{
+		"neovim/nvim-lspconfig",
+		cmd = { "LspInfo", "LspInstall", "LspStart" },
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "williamboman/mason-lspconfig.nvim" },
+		},
+		config = function()
+			vim.lsp.set_log_level("off")
+			-- This is where all the LSP shenanigans will live
+			local lsp_zero = require("lsp-zero")
+			lsp_zero.extend_lspconfig()
 
-            lsp_zero.format_on_save({
-                format_opts = {
-                    async = true,
-                    timeout_ms = 10000,
-                },
-                servers = {
-                    ['gopls'] = { 'go' },
-                }
-            })
+			lsp_zero.format_on_save({
+				format_opts = {
+					async = true,
+					timeout_ms = 10000,
+				},
+				servers = {
+					["gopls"] = { "go" },
+				},
+			})
 
+			--- if you want to know more about lsp-zero and mason.nvim
+			--- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+			lsp_zero.on_attach(function(client, bufnr)
+				-- disable lsp syntax highlighting
+				client.server_capabilities.semanticTokensProvider = nil
+				-- see :help lsp-zero-keybindings
+				-- to learn the available actions
+				lsp_zero.default_keymaps({ buffer = bufnr })
+			end)
 
-            --- if you want to know more about lsp-zero and mason.nvim
-            --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-            lsp_zero.on_attach(function(client, bufnr)
-                -- disable lsp syntax highlighting
-                client.server_capabilities.semanticTokensProvider = nil
-                -- see :help lsp-zero-keybindings
-                -- to learn the available actions
-                lsp_zero.default_keymaps({buffer = bufnr})
-            end)
-
-            require('mason-lspconfig').setup({
-                ensure_installed = {},
-                handlers = {
-                    lsp_zero.default_setup,
-                    lua_ls = function()
-                        -- (Optional) Configure lua language server for neovim
-                        local lua_opts = lsp_zero.nvim_lua_ls()
-                        require('lspconfig').lua_ls.setup(lua_opts)
-                    end,
-                }
-            })
-        end
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        event = { "BufReadPost", "BufNewFile" },
-        opts = {
-            highlight = {
-                enable = true
-            },
-            indent = {
-                enable = false
-            },
-            context_commentstring = {
-                enable = true,
-                enable_autocmd = false
-            },
-        },
-        config = function(_, opts)
-            require("nvim-treesitter.configs").setup(opts)
-        end,
-    },
-    {
-        'numToStr/Comment.nvim',
-        config = function()
-            require('Comment').setup()
-        end,
-    },
-    {
-        "j-hui/fidget.nvim",
-        -- tag = "legacy",
-        config = function(_, opts)
-            require("fidget").setup(opts)
-        end
-    },
-    {
-        "echasnovski/mini.indentscope",
-        event = { "BufReadPre", "BufNewFile" },
-        opts = {
-            symbol = "│",
-            options = { try_as_border = true },
-        },
-    },
-    -- { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
+			require("mason-lspconfig").setup({
+				ensure_installed = {},
+				handlers = {
+					lsp_zero.default_setup,
+					lua_ls = function()
+						-- (Optional) Configure lua language server for neovim
+						local lua_opts = lsp_zero.nvim_lua_ls()
+						require("lspconfig").lua_ls.setup(lua_opts)
+					end,
+				},
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			highlight = {
+				enable = true,
+			},
+			indent = {
+				enable = false,
+			},
+			context_commentstring = {
+				enable = true,
+				enable_autocmd = false,
+			},
+		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
+	{
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	},
+	{
+		"j-hui/fidget.nvim",
+		-- tag = "legacy",
+		config = function(_, opts)
+			require("fidget").setup(opts)
+		end,
+	},
+	{
+		"echasnovski/mini.indentscope",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			symbol = "│",
+			options = { try_as_border = true },
+		},
+	},
+	-- { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
 })
 
 ----------------------------------------
@@ -185,13 +185,13 @@ require("lazy").setup({
 ----------------------------------------
 
 vim.cmd.colorscheme("base16-default-dark") -- colorscheme
-vim.opt.number = true                      -- Print line number
+vim.opt.number = true -- Print line number
 -- vim.opt.relativenumber = true -- Relative line numbers
-vim.opt.termguicolors = true               -- True color support
-vim.opt.splitright = true                  -- Put new windows right of current
-vim.opt.splitbelow = true                  -- Put new windows below of current
+vim.opt.termguicolors = true -- True color support
+vim.opt.splitright = true -- Put new windows right of current
+vim.opt.splitbelow = true -- Put new windows below of current
 vim.opt.grepprg = "rg --vimgrep"
-vim.opt.ignorecase = true                  -- Ignore case
+vim.opt.ignorecase = true -- Ignore case
 
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
@@ -227,10 +227,10 @@ keymap.set("n", "<leader>ca", '<cmd>lua require("fzf-lua").lsp_code_actions({ sy
 keymap.set("n", "<C-k>", [[<Cmd>lua require"fzf-lua".builtin()<CR>]], {})
 keymap.set("n", "gx", [[<Cmd>silent !open <cWORD><CR>]], {})
 
-keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>')
-keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
-keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>")
+keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
+keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
+keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
 
 keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
@@ -238,27 +238,20 @@ keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 -- functions
 ----------------------------------------
 
-vim.api.nvim_create_autocmd(
-    'TermOpen',
-    {
-        pattern = '*',
-        command = 'startinsert'
-    }
-)
+vim.api.nvim_create_autocmd("TermOpen", {
+	pattern = "*",
+	command = "startinsert",
+})
 
-vim.api.nvim_create_user_command('PR', 'terminal gh pr create', { nargs = 0 })
+vim.api.nvim_create_user_command("PR", "terminal gh pr create", { nargs = 0 })
 
-vim.api.nvim_create_user_command('GPush', 'Git --no-pager push', { nargs = 0})
-vim.api.nvim_create_user_command('GPull', 'Git --no-pager pull', { nargs = 0})
+vim.api.nvim_create_user_command("GPush", "Git --no-pager push", { nargs = 0 })
+vim.api.nvim_create_user_command("GPull", "Git --no-pager pull", { nargs = 0 })
 
 -- To make GBrowse work with oil.nvim without netrw
-vim.api.nvim_create_user_command(
-  'Browse',
-  function (opts)
-    vim.fn.system { 'open', opts.fargs[1] }
-  end,
-  { nargs = 1 }
-)
+vim.api.nvim_create_user_command("Browse", function(opts)
+	vim.fn.system({ "open", opts.fargs[1] })
+end, { nargs = 1 })
 
 ----------------------------------------
 -- statuslines
@@ -266,13 +259,13 @@ vim.api.nvim_create_user_command(
 local fn, cmd = vim.fn, vim.cmd
 
 function my_statusline()
-  local branch = fn.FugitiveHead()
+	local branch = fn.FugitiveHead()
 
-  if branch and #branch > 0 then
-    branch = '  '..branch
-  end
+	if branch and #branch > 0 then
+		branch = "  " .. branch
+	end
 
-  return ' %f%m%=%l:%c'..branch
+	return " %f%m%=%l:%c" .. branch
 end
 
-cmd[[ set statusline=%!luaeval('my_statusline()') ]]
+cmd([[ set statusline=%!luaeval('my_statusline()') ]])
--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,19 +7,19 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Default Dark (base16)'
+config.color_scheme = "Default Dark (base16)"
 
 config.use_fancy_tab_bar = false
-config.font_size = 14
+config.font_size = 13
 -- config.font = 'Monaspace Argon'
 config.hide_tab_bar_if_only_one_tab = true
-config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 -- and finally, return the configuration to wezterm
 return config
