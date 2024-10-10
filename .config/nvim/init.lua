025e25065df23237f64b857ed4b98eb8ff7786d9
Yong Su Lee <yslee3@icloud.com>
2024-10-10 13:45:11 -0400

    dont use leap anymore

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -204,23 +204,6 @@ require("lazy").setup({
         ---@type gopher.Config
         opts = {},
     },
-    {
-        "ggandor/leap.nvim",
-        enabled = true,
-        keys = {
-            { "s", mode = { "n", "x", "o" }, desc = "Leap Forward to" },
-            { "S", mode = { "n", "x", "o" }, desc = "Leap Backward to" },
-        },
-        config = function(_, opts)
-            local leap = require("leap")
-            for k, v in pairs(opts) do
-                leap.opts[k] = v
-            end
-            leap.add_default_mappings(true)
-            vim.keymap.del({ "x", "o" }, "x")
-            vim.keymap.del({ "x", "o" }, "X")
-        end,
-    },
 })
 
 ----------------------------------------
