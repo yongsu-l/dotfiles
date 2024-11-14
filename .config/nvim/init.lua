82c9cc4f44190939706df88bd4e859750cab34d9
Yong Su Lee <yslee3@icloud.com>
2024-11-13 22:14:25 -0500

    move around configurations

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,129 @@
+----------------------------------------
+-- options
+----------------------------------------
+
+vim.opt.number = true -- Print line number
+vim.opt.termguicolors = true -- True color support
+vim.opt.splitright = true -- Put new windows right of current
+vim.opt.splitbelow = true -- Put new windows below of current
+vim.opt.grepprg = "rg --vimgrep"
+vim.opt.ignorecase = true -- Ignore case
+
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+
+vim.opt.updatetime = 100
+
+-- Disable mouse
+vim.opt.mouse = nil
+
+-- Disable swap
+vim.opt.swapfile = false
+
+----------------------------------------
+-- keymaps
+----------------------------------------
+local keymap = vim.keymap
+
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+keymap.set("n", "<leader><space>", ":noh<CR>", { desc = "Clear highlight" })
+keymap.set("v", "p", "pgvy", { desc = "Paste in visual mode without copying" })
+
+keymap.set("n", "<leader>f", "<cmd>FzfLua files<CR>")
+keymap.set("n", "<leader>b", "<cmd>FzfLua buffers<CR>")
+keymap.set("n", "<leader>/", "<cmd>FzfLua live_grep_glob<CR>")
+keymap.set("n", "<leader>,", "<cmd>FzfLua resume<CR>")
+
+keymap.set("n", "<leader>v", "<cmd>FzfLua grep_cword<CR>")
+keymap.set("v", "<leader>v", "<cmd>FzfLua grep_visual<CR>")
+
+keymap.set("n", "gd", '<cmd>lua require("fzf-lua").lsp_definitions({ jump_to_single_result = true })<CR>')
+keymap.set("n", "gD", '<cmd>lua require("fzf-lua").lsp_declarations({ jump_to_single_result = true })<CR>')
+keymap.set("n", "gi", '<cmd>lua require("fzf-lua").lsp_implementations({ jump_to_single_result = true })<CR>')
+keymap.set("n", "gr", '<cmd>lua require("fzf-lua").lsp_references({ ignore_current_line = true })<CR>')
+keymap.set("n", "<leader>ca", '<cmd>lua require("fzf-lua").lsp_code_actions({ sync = true })<cr>')
+keymap.set("n", "<C-k>", [[<Cmd>lua require"fzf-lua".builtin()<CR>]], {})
+keymap.set("n", "gx", [[<Cmd>silent !open <cWORD><CR>]], {})
+
+keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>")
+keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
+keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
+keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
+
+keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+----------------------------------------
+-- LSP
+----------------------------------------
+vim.diagnostic.config({
+	virtual_text = false, -- turn off inline diagnostics
+})
+
+vim.api.nvim_create_augroup("diagnostics", { clear = true })
+
+-- automatically populate loclisfunctionst
+vim.api.nvim_create_autocmd("DiagnosticChanged", {
+	group = "diagnostics",
+	callback = function()
+		vim.diagnostic.setloclist({ open = false })
+	end,
+})
+
+for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+	vim.api.nvim_set_hl(0, group, {})
+end
+
+----------------------------------------
+-- functions
+----------------------------------------
+
+vim.api.nvim_create_autocmd("TermOpen", {
+	pattern = "*",
+	command = "startinsert",
+})
+
+vim.api.nvim_create_user_command("PR", "terminal gh pr create", { nargs = 0 })
+
+vim.api.nvim_create_user_command("GPush", "Git --no-pager push", { nargs = 0 })
+vim.api.nvim_create_user_command("GPull", "Git --no-pager pull", { nargs = 0 })
+
+-- To make GBrowse work with oil.nvim without netrw
+vim.api.nvim_create_user_command("Browse", function(opts)
+	vim.fn.system({ "open", opts.fargs[1] })
+end, { nargs = 1 })
+
+----------------------------------------
+-- statuslines
+----------------------------------------
+local fn = vim.fn
+
+function my_statusline()
+	local branch = fn.FugitiveHead()
+
+	if branch and #branch > 0 then
+		branch = "  " .. branch
+	end
+
+	return " %f%m%=%l:%c" .. branch
+end
+
+vim.opt.statusline = "%!v:lua.my_statusline()"
+
+-- Resize window when we resize the terminal / tmux
+vim.api.nvim_command("autocmd VimResized * wincmd =")
+
+vim.api.nvim_create_augroup("diagnostics", { clear = true })
+
+vim.api.nvim_create_autocmd("DiagnosticChanged", {
+	group = "diagnostics",
+	callback = function()
+		vim.diagnostic.setloclist({ open = false })
+	end,
+})
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -12,7 +138,12 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{ "RRethy/nvim-base16" },
+	{
+		"RRethy/nvim-base16",
+		config = function()
+			vim.cmd.colorscheme("base16-default-dark") -- colorscheme
+		end,
+	},
 	{
 		"stevearc/oil.nvim",
 		config = function()
@@ -209,132 +340,4 @@ require("lazy").setup({
 		end,
 		opts = {},
 	},
-})
-
-----------------------------------------
--- options
-----------------------------------------
-
-vim.cmd.colorscheme("base16-default-dark") -- colorscheme
-vim.opt.number = true -- Print line number
-vim.opt.termguicolors = true -- True color support
-vim.opt.splitright = true -- Put new windows right of current
-vim.opt.splitbelow = true -- Put new windows below of current
-vim.opt.grepprg = "rg --vimgrep"
-vim.opt.ignorecase = true -- Ignore case
-
-vim.opt.smartindent = true
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
-vim.opt.tabstop = 4
-
-vim.opt.updatetime = 100
-
--- Disable mouse
-vim.opt.mouse = nil
-
--- Disable swap
-vim.opt.swapfile = false
-
-----------------------------------------
--- keymaps
-----------------------------------------
-local keymap = vim.keymap
-
-vim.g.mapleader = ","
-vim.g.maplocalleader = ","
-keymap.set("n", "<leader><space>", ":noh<CR>", { desc = "Clear highlight" })
-keymap.set("v", "p", "pgvy", { desc = "Paste in visual mode without copying" })
-
-keymap.set("n", "<leader>f", "<cmd>FzfLua files<CR>")
-keymap.set("n", "<leader>b", "<cmd>FzfLua buffers<CR>")
-keymap.set("n", "<leader>/", "<cmd>FzfLua live_grep_glob<CR>")
-keymap.set("n", "<leader>,", "<cmd>FzfLua resume<CR>")
-
-keymap.set("n", "<leader>v", "<cmd>FzfLua grep_cword<CR>")
-keymap.set("v", "<leader>v", "<cmd>FzfLua grep_visual<CR>")
-
-keymap.set("n", "gd", '<cmd>lua require("fzf-lua").lsp_definitions({ jump_to_single_result = true })<CR>')
-keymap.set("n", "gD", '<cmd>lua require("fzf-lua").lsp_declarations({ jump_to_single_result = true })<CR>')
-keymap.set("n", "gi", '<cmd>lua require("fzf-lua").lsp_implementations({ jump_to_single_result = true })<CR>')
-keymap.set("n", "gr", '<cmd>lua require("fzf-lua").lsp_references({ ignore_current_line = true })<CR>')
-keymap.set("n", "<leader>ca", '<cmd>lua require("fzf-lua").lsp_code_actions({ sync = true })<cr>')
-keymap.set("n", "<C-k>", [[<Cmd>lua require"fzf-lua".builtin()<CR>]], {})
-keymap.set("n", "gx", [[<Cmd>silent !open <cWORD><CR>]], {})
-
-keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>")
-keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
-keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
-keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
-
-keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-
-----------------------------------------
--- LSP
-----------------------------------------
-vim.diagnostic.config({
-	virtual_text = false, -- turn off inline diagnostics
-})
-
-vim.api.nvim_create_augroup("diagnostics", { clear = true })
-
--- automatically populate loclisfunctionst
-vim.api.nvim_create_autocmd("DiagnosticChanged", {
-	group = "diagnostics",
-	callback = function()
-		vim.diagnostic.setloclist({ open = false })
-	end,
-})
-
-for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
-	vim.api.nvim_set_hl(0, group, {})
-end
-
-----------------------------------------
--- functions
-----------------------------------------
-
-vim.api.nvim_create_autocmd("TermOpen", {
-	pattern = "*",
-	command = "startinsert",
-})
-
-vim.api.nvim_create_user_command("PR", "terminal gh pr create", { nargs = 0 })
-
-vim.api.nvim_create_user_command("GPush", "Git --no-pager push", { nargs = 0 })
-vim.api.nvim_create_user_command("GPull", "Git --no-pager pull", { nargs = 0 })
-
--- To make GBrowse work with oil.nvim without netrw
-vim.api.nvim_create_user_command("Browse", function(opts)
-	vim.fn.system({ "open", opts.fargs[1] })
-end, { nargs = 1 })
-
-----------------------------------------
--- statuslines
-----------------------------------------
-local fn = vim.fn
-
-function my_statusline()
-	local branch = fn.FugitiveHead()
-
-	if branch and #branch > 0 then
-		branch = "  " .. branch
-	end
-
-	return " %f%m%=%l:%c" .. branch
-end
-
-vim.opt.statusline = "%!v:lua.my_statusline()"
-
--- Resize window when we resize the terminal / tmux
-vim.api.nvim_command("autocmd VimResized * wincmd =")
-
-vim.api.nvim_create_augroup("diagnostics", { clear = true })
-
-vim.api.nvim_create_autocmd("DiagnosticChanged", {
-	group = "diagnostics",
-	callback = function()
-		vim.diagnostic.setloclist({ open = false })
-	end,
 })
