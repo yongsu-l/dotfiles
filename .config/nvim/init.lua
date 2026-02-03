b9d63f1a292da418504d8a088b37a2b0fafa25c1
Yong Su Lee <yslee3@icloud.com>
2026-02-03 09:45:57 -0500

    i dunno

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -240,21 +240,21 @@ require("lazy").setup({
 		event = { "BufReadPost", "BufNewFile" },
 		lazy = false,
 		build = ":TSUpdate",
-		opts = {
-			highlight = {
-				enable = true,
-			},
-			indent = {
-				enable = true,
-			},
-			context_commentstring = {
-				enable = true,
-			},
-			ensure_installed = { "go", "lua", "vim", "vimdoc", "typescript", "ruby", "rust" },
-		},
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
-		end,
+		-- opts = {
+		-- 	highlight = {
+		-- 		enable = true,
+		-- 	},
+		-- 	indent = {
+		-- 		enable = true,
+		-- 	},
+		-- 	context_commentstring = {
+		-- 		enable = true,
+		-- 	},
+		-- 	ensure_installed = { "go", "lua", "vim", "vimdoc", "typescript", "ruby", "rust" },
+		-- },
+		-- config = function(_, opts)
+		-- 	require("nvim-treesitter.configs").setup(opts)
+		-- end,
 	},
 	{
 		"stevearc/conform.nvim",
